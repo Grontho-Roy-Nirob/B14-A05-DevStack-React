995e5e7b7541819b76cb9ddbfa2330603d17f884
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-09-11 00:55:34 +0600

    Readme Modified

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-red-800">Welcome to Vite + React</h2>
+      <button className="btn btn-secondary">Secondary</button>
+    </>
+  )
+}
+
+export default App
