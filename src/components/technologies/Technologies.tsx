a738383937a25b304d821bb611fd4d6f42875ae7
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-09-11 15:10:13 +0600

    Color modified

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import type { ITechnology } from "../../types/technology";
 import TechnologyCard from "./TechnologyCard";
 
+
 interface ITechnologiesProps {
   technologies: ITechnology[];
 }
@@ -17,7 +18,7 @@ const Technologies = ({ technologies }: ITechnologiesProps) => {
       <div className="mb-8">
         <h2 className="text-3xl font-extrabold text-slate-900">
           Explore the{" "}
-          <span className="bg-gradient-to-r from-orange-500 via-pink-500 to-violet-600 bg-clip-text text-transparent">
+          <span className="text-pink-500">
             Technologies
           </span>
         </h2>
